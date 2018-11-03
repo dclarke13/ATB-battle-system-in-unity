969ec5c392d2e7ec9b0e813953d91e1f5ee7d787
Darren Clarke <darrc13@gmail.com>
2018-11-03 17:20:39 +0000

    basic overworld and player movement added

---
+++ Turn Based Battle/Assets/Scripts/Gmanager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gmanager : MonoBehaviour {
+
+    public static Gmanager instance;
+	// Use this for initialization
+	void Awake () {
+		
+        //check if the instance exists
+        if(instance == null)
+        {
+            //if not set instance to this GameObject
+            instance = this;
+        }
+        //if exists but is not this instance
+        else if(instance != this)
+        {
+            //destroy it to prevent duplicate objects
+            Destroy(gameObject);
+        }
+        //set current to not destroyable
+        DontDestroyOnLoad(gameObject);
+    }
+	
+}
---
+++ Turn Based Battle/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float moveSpeed = 10f;
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX,0.0f,moveZ);
+        GetComponent<Rigidbody>().velocity = movement * moveSpeed;//* Time.deltaTime;
+
+	}
+}
